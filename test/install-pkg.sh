20587f9f2b3baac125031b1f99994070db2cd1c1
Joe Mullally <jwmullally@gmail.com>
2020-05-04 11:07:54 +0100

    Small improvements to scripts

--- test/install-pkg.sh
+++ test/install-pkg.sh
@@ -18,6 +18,6 @@ $run_cmd rm -f dracut-iscsi-target-*.rpm
 sshpass -e scp $sshopts $(find ../build/ -type f -name "*.noarch.rpm") "root@$target_ip:"
 $run_cmd dnf install -y dracut-iscsi-target-*.rpm
 $run_cmd sed -i 's/^#//' /etc/dracut.conf.d/iscsi-target.conf
-$run_cmd /bin/kernel-install add "\$(uname -r)" "/lib/modules/\$(uname -r)/vmlinuz"
+$run_cmd /bin/kernel-install --verbose add "\$(uname -r)" "/lib/modules/\$(uname -r)/vmlinuz"
 [ ! -e /var/tmp/iscsi-boot.iso ] || sudo rm /var/tmp/iscsi-boot.iso
 sshpass -e scp $sshopts "root@$target_ip:/boot/iscsi-boot-*.iso" /var/tmp/iscsi-boot.iso
